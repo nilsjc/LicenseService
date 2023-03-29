cb81a6e95d2c593533cdbfb0c94f4fa3c2794997
Edvardsson, Nils <nils.edvardsson@if.se>
2023-03-29 20:46:16 +0200

    Added admin table

--- InstantLicenses/Client/instantLicenses.client/instantlicenses.client/src/admin.js
+++ InstantLicenses/Client/instantLicenses.client/instantlicenses.client/src/admin.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 
 function EnterAdminMode(props) {
     const [size, setSize] = useState(25);
-    const [licenseTable, setLicenseTable] = useState([{ name: "Anom", age: 19, gender: "Male" }]);
+    const [licenseTable, setLicenseTable] = useState([]);
 
     const watchData = () => {
         fetch(`https://localhost:7292/api/Admin?size=${size}`,
@@ -12,16 +12,25 @@ function EnterAdminMode(props) {
             })
             .then(data => data.json())
             .then(data => {
-                licenseTable = data;
+                console.log(data);
+                let dataArray = [];
+
+                data.forEach(list => {
+                    dataArray.push({
+                        license: list["name"],
+                        customer: list["rentCustomer"],
+                        timeLeft: list["timeLeft"].toString()
+                        })
+                    }
+                )
+                console.log(dataArray);
+                setLicenseTable(dataArray);
             }).catch(e => {
                 console.log(e);
             });
     }
     return (
         <div className="App">
-            <p>
-                <button onClick={watchData}>Update Table</button>
-            </p>
             <table>
                 <tr>
                     <th>License</th>
@@ -31,13 +40,16 @@ function EnterAdminMode(props) {
                 {licenseTable.map((val, key) => {
                     return (
                         <tr key={key}>
-                            <td>{val.name}</td>
-                            <td>{val.age}</td>
-                            <td>{val.gender}</td>
+                            <td>{val.license}</td>
+                            <td>{val.customer}</td>
+                            <td>{val.timeLeft}</td>
                         </tr>
                     )
                 })}
             </table>
+            <p>
+                <button onClick={watchData}>Update Table</button>
+            </p>
         </div>
     )
     
